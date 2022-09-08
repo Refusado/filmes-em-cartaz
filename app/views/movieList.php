c5280c98851e5156149f0a1a1cfc7b461f8cb65c
Renan <orefusado@gmail.com>
2022-09-08 19:14:22 -0300

    melhorias responsividade

--- app/views/movieList.php
+++ app/views/movieList.php
@@ -12,22 +12,22 @@ function showMoviesList() {
     $rating = $movie->getRating();
     switch ($rating) {
       case "L":
-        $ratingIcon = "bg-green-600";
+        $ratingBg = "bg-green-600";
         break;
       case "10":
-        $ratingIcon = "bg-blue-500";
+        $ratingBg = "bg-blue-500";
         break;
       case "12":
-        $ratingIcon = "bg-yellow-500";
+        $ratingBg = "bg-yellow-500";
         break;
       case "14":
-        $ratingIcon = "bg-orange-400";
+        $ratingBg = "bg-orange-400";
         break;
       case "16":
-        $ratingIcon = "bg-red-600";
+        $ratingBg = "bg-red-600";
         break;
       case "18":
-        $ratingIcon = "bg-neutral-800";
+        $ratingBg = "bg-neutral-800";
         break;
     } 
     $trailerUrl = $movie->getTrailer();
@@ -40,21 +40,21 @@ function showMoviesList() {
     
     
     echo "
-    <div class='w-10/12 md:w-7/12 lg:w-96 h-52 flex overflow-hidden border-t-2 border-red-600 lg:border-red-800 lg:hover:border-red-600 duration-500 group bg-gradient-to-r from-zinc-800'>
+    <div class='w-10/12 md:w-7/12 lg:w-96 h-52 flex border-t-2 border-red-600 lg:border-red-800 lg:hover:border-red-600 duration-500 group bg-gradient-to-r from-zinc-800'>
       <img class='h-full inline-block bg-black' src='$imgUrl' alt='$name' title='$name'/>
-      <div class='p-5 flex flex-col justify-start'>
+      <div class='p-5 pr-0 flex flex-col justify-start'>
         <div class='max-w-full max-h-18 pb-3 md:pb-5'>
           <h3 class='text-lg text-zinc-300 tracking-wide leading-5 '>$name</h3>
         </div>
         <div class='py-2 mb-3 text-xs font-bold text-zinc-300'>$trailer</div>
 
-        <div class='mt-auto flex justify-start items-center flex-wrap'>
-          <span class='${ratingIcon} select-none text-sm font-bold text-zinc-300 flex justify-center items-center h-8 w-8 rounded border border-zinc-500
+        <div class='mt-auto flex justify-start items-center flex-nowrap'>
+          <span class='${ratingBg} select-none text-sm font-bold text-zinc-300 flex justify-center items-center h-8 w-8 p-1.5 min-w-fit rounded border border-zinc-500
           lg:opacity-0 lg:-translate-x-6 lg:group-hover:opacity-90 lg:group-hover:-translate-x-0 duration-500'
             title='Classificação indicativa de $rating anos'>
             $rating
           </span>
-          <a class='p-2 underline uppercase ml-6 text-red-600 hover:text-red-500 tracking-wide cursor-pointer
+          <a class='p-2 text-sm md:text-base underline uppercase ml-6 text-red-600 hover:text-red-500 tracking-wide cursor-pointer
           lg:opacity-0 lg:-translate-x-6 lg:group-hover:opacity-90 lg:group-hover:-translate-x-0 duration-500 delay-100'>Saiba mais</a>
         </div>
       </div>
