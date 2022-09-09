a8d4b9e93a66cd6bdbe03170dc3a659d5dca52c4
Renan <orefusado@gmail.com>
2022-09-08 21:01:07 -0300

    coletando dados dos filmes + melhorias

--- app/views/movieList.php
+++ app/views/movieList.php
@@ -35,7 +35,7 @@ function showMoviesList() {
     "<a class='bg-zinc-200 text-zinc-700 select-none cursor-pointer border border-zinc-300 inline rounded-sm py-2 px-3 lg:shadow-md hover:shadow-zinc-400 opacity-90
     duration-200' href='$trailerUrl' target='_blank'>TRAILER</a>" :
     
-    "<p class='bg-zinc-800                 select-none cursor-default border border-zinc-300 inline rounded-sm py-2 px-3 opacity-50'
+    "<p class='bg-zinc-800                 select-none cursor-default border border-zinc-300 inline rounded-sm py-2 px-3 opacity-30'
     title='Sem trailer disponível'>TRAILER</p>";
     
     
@@ -44,17 +44,17 @@ function showMoviesList() {
       <img class='h-full inline-block bg-black' src='$imgUrl' alt='$name' title='$name'/>
       <div class='p-5 pr-0 flex flex-col justify-start'>
         <div class='max-w-full max-h-18 pb-3 md:pb-5'>
-          <h3 class='text-lg text-zinc-300 tracking-wide leading-5 '>$name</h3>
+          <h3 class='text-lg text-zinc-300 opacity-80 tracking-wide leading-5 '>$name</h3>
         </div>
         <div class='py-2 mb-3 text-xs font-bold text-zinc-300'>$trailer</div>
 
         <div class='mt-auto flex justify-start items-center flex-nowrap'>
           <span class='${ratingBg} select-none text-sm font-bold text-zinc-300 flex justify-center items-center h-8 w-8 p-1.5 min-w-fit rounded border border-zinc-500
-          lg:opacity-0 lg:-translate-x-6 lg:group-hover:opacity-90 lg:group-hover:-translate-x-0 duration-500'
+          lg:opacity-0 lg:-translate-x-6 lg:group-hover:opacity-100 lg:group-hover:-translate-x-0 duration-500'
             title='Classificação indicativa de $rating anos'>
             $rating
           </span>
-          <a class='p-2 text-sm md:text-base underline uppercase ml-6 text-red-600 hover:text-red-500 tracking-wide cursor-pointer
+          <a class='p-2 text-sm md:text-base underline uppercase ml-6 text-red-500 opacity-70 tracking-widest cursor-pointer
           lg:opacity-0 lg:-translate-x-6 lg:group-hover:opacity-90 lg:group-hover:-translate-x-0 duration-500 delay-100'>Saiba mais</a>
         </div>
       </div>
